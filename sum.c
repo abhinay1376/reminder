fbc40ff1b8f9ad6396abd77e9196863a9224bc6c
abhinay1376 <abhinay1376@gmail.com>
2025-07-06 12:16:54 +0530

    sum.c

---
+++ sum.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2, sum;
+
+    // Input from user
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    // Calculate sum
+    sum = num1 + num2;
+
+    // Output result
+    printf("Sum = %d\n", sum);
+
+    return 0;
+}
